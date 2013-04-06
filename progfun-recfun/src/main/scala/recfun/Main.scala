a98d204d981164947c94a1e76147f901eee8d39c
Emanuel <campos.e@gmail.com>
2013-04-06 19:21:08 +0200

    add stuff

--- progfun-recfun/src/main/scala/recfun/Main.scala
+++ progfun-recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,32 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
-
+  def pascal(c: Int, r: Int): Int =   {
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def brackets(chars: List[Char], balancer: Int): Boolean =
+      if (chars.isEmpty) balancer ==0
+      else if (chars.head == '(') brackets(chars.tail, balancer + 1)
+      else if (chars.head == ')') balancer > 0 && brackets(chars.tail, balancer - 1)
+      else brackets(chars.tail, balancer)
+    brackets(chars, 0)
+  } 
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =  {
+  if(money == 0) 1
+  else if (money < 0) 0
+  else if (coins.isEmpty) 0
+  else {
+    countChange(money - coins.head, coins) +
+    countChange(money, coins.tail)
+  }
+}
 }
